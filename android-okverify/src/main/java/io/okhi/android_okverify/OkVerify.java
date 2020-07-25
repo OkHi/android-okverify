b9558047daeeaf144381850193b168f96aa4776b
Julius Kiano <jgkiano@gmail.com>
2020-07-25 11:10:07 +0300

    added start methods

--- android-okverify/src/main/java/io/okhi/android_okverify/OkVerify.java
+++ android-okverify/src/main/java/io/okhi/android_okverify/OkVerify.java
@@ -9,21 +9,31 @@ import androidx.annotation.NonNull;
 import java.util.Objects;
 
 import io.okhi.android_background_geofencing.interfaces.RequestHandler;
-import io.okhi.android_background_geofencing.models.BackgroundGeofence;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingException;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingLocationService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPermissionService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPlayService;
 import io.okhi.android_core.OkHiCore;
+import io.okhi.android_core.interfaces.OkHiSignInRequestHandler;
 import io.okhi.android_core.models.OkHiAuth;
 import io.okhi.android_core.models.OkHiException;
+import io.okhi.android_core.models.OkHiLocation;
+import io.okhi.android_core.models.OkHiMode;
+import io.okhi.android_core.models.OkHiUser;
+import io.okhi.android_okverify.interfaces.OkVerifyAsyncTaskHandler;
+import io.okhi.android_okverify.interfaces.OkVerifyCallback;
 import io.okhi.android_okverify.interfaces.OkVerifyRequestHandler;
+import io.okhi.android_okverify.models.Constant;
+import io.okhi.android_okverify.models.OkVerifyGeofence;
 
 public class OkVerify extends OkHiCore {
     private Activity activity;
+    private OkHiAuth auth;
     private BackgroundGeofencingPermissionService permissionService;
     private BackgroundGeofencingPlayService playService;
     private BackgroundGeofencingLocationService locationService;
+    private String TRANSIT_URL;
+    private String TRANSIT_CONFIG_URL;
 
     private static class BackgroundGeofenceRequestHandler implements RequestHandler {
         private OkVerifyRequestHandler requestHandler;
@@ -43,17 +53,25 @@ public class OkVerify extends OkHiCore {
     private OkVerify(@NonNull Builder builder) {
         super(builder.auth);
         this.activity = builder.activity;
+        this.auth = builder.auth;
+        if (builder.auth.getContext().getMode().equals(OkHiMode.DEV)) {
+            TRANSIT_URL = Constant.DEV_BASE_URL + Constant.TRANSIT_ENDPOINT;
+            TRANSIT_CONFIG_URL = Constant.DEV_BASE_URL + Constant.TRANSIT_CONFIG_ENDPOINT;
+        } else if (builder.auth.getContext().getMode().equals(OkHiMode.PROD)) {
+            TRANSIT_URL = Constant.PROD_BASE_URL + Constant.TRANSIT_ENDPOINT;
+            TRANSIT_CONFIG_URL = Constant.PROD_BASE_URL + Constant.TRANSIT_CONFIG_ENDPOINT;
+        } else {
+            TRANSIT_URL = Constant.SANDBOX_BASE_URL + Constant.TRANSIT_ENDPOINT;
+            TRANSIT_CONFIG_URL = Constant.SANDBOX_BASE_URL + Constant.TRANSIT_CONFIG_ENDPOINT;
+        }
     }
 
     public static class Builder {
         private OkHiAuth auth;
         private Activity activity;
-        public Builder(@NonNull OkHiAuth auth) {
+        public Builder(@NonNull OkHiAuth auth, Activity activity) {
             this.auth = auth;
-        }
-        public Builder setActivity(@NonNull Activity activity) {
             this.activity = activity;
-            return this;
         }
         public OkVerify build() {
             return new OkVerify(this);
@@ -110,5 +128,45 @@ public class OkVerify extends OkHiCore {
         if (locationService != null) {
             locationService.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    public void start(OkHiUser user, final OkHiLocation location, final OkVerifyCallback<String> handler) {
+        anonymousSignWithPhoneNumber(user.getPhone(), Constant.OKVERIFY_SCOPES, new OkHiSignInRequestHandler() {
+            @Override
+            public void onSuccess(String authorizationToken) {
+                start(activity.getApplicationContext(), authorizationToken, location, handler);
+            }
+            @Override
+            public void onError(OkHiException exception) {
+                handler.onError(exception);
+            }
+        });
+    }
+
+
+    private void start(final Context context, final String authorizationToken, final OkHiLocation location, final OkVerifyCallback<String> handler) {
+        OkVerifyGeofence.getGeofence(context, authorizationToken, auth.getAccessToken(), TRANSIT_CONFIG_URL, TRANSIT_URL, new OkVerifyAsyncTaskHandler<OkVerifyGeofence>() {
+            @Override
+            public void onSuccess(OkVerifyGeofence geofence) {
+                start(context, geofence, location, handler);
+            }
+            @Override
+            public void onError(OkHiException exception) {
+                handler.onError(exception);
+            }
+        });
+    }
+
+    private void start(Context context, OkVerifyGeofence geofence, OkHiLocation location, final OkVerifyCallback<String> handler) {
+        geofence.start(context, location.getId(), location.getLat(), location.getLon(), new OkVerifyAsyncTaskHandler<String>() {
+            @Override
+            public void onSuccess(String result) {
+                handler.onSuccess(result);
+            }
+            @Override
+            public void onError(OkHiException exception) {
+                handler.onError(exception);
+            }
+        });
     }
 }
