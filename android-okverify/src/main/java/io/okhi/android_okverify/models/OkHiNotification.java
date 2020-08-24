d494f4419e9b06aa5718facb66b15fa2f9f3fcd8
Julius Kiano <jgkiano@gmail.com>
2020-08-24 20:26:08 +0300

    defined OkHi Notification

--- android-okverify/src/main/java/io/okhi/android_okverify/models/OkHiNotification.java
+++ android-okverify/src/main/java/io/okhi/android_okverify/models/OkHiNotification.java
@@ -2,10 +2,50 @@ package io.okhi.android_okverify.models;
 
 import androidx.annotation.NonNull;
 
-import io.okhi.android_background_geofencing.models.BackgroundGeofencingNotification;
+public class OkHiNotification {
+    private String title;
+    private String text;
+    private int channelImportance;
+    private String channelId;
+    private String channelName;
+    private String channelDescription;
+    private int icon = 0;
 
-public class OkHiNotification extends BackgroundGeofencingNotification {
     public OkHiNotification(@NonNull String title, @NonNull String text, @NonNull String channelId, @NonNull String channelName, @NonNull String channelDescription, int channelImportance, int icon) {
-        super(title, text, channelId, channelName, channelDescription, channelImportance, icon);
+        this.title = title;
+        this.text = text;
+        this.channelId = channelId;
+        this.channelName = channelName;
+        this.channelDescription = channelDescription;
+        this.channelImportance = channelImportance;
+        this.icon = icon;
+    }
+
+    public int getChannelImportance() {
+        return channelImportance;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getChannelDescription() {
+        return channelDescription;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
