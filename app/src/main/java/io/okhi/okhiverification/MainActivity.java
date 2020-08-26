ce296ae4592d000e3763d3cd0b09cbf765275fc0
Julius Kiano <jgkiano@gmail.com>
2020-08-26 21:23:32 +0300

    use core library for permission, location and play services

--- app/src/main/java/io/okhi/okhiverification/MainActivity.java
+++ app/src/main/java/io/okhi/okhiverification/MainActivity.java
@@ -1,15 +1,18 @@
 package io.okhi.okhiverification;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.okhi.android_core.OkHi;
+import io.okhi.android_core.interfaces.OkHiRequestHandler;
 import io.okhi.android_core.models.OkHiAppContext;
 import io.okhi.android_core.models.OkHiAuth;
 import io.okhi.android_core.models.OkHiException;
@@ -17,15 +20,17 @@ import io.okhi.android_core.models.OkHiLocation;
 import io.okhi.android_core.models.OkHiUser;
 import io.okhi.android_okverify.OkVerify;
 import io.okhi.android_okverify.interfaces.OkVerifyCallback;
-import io.okhi.android_okverify.interfaces.OkVerifyRequestHandler;
 import io.okhi.android_okverify.models.OkHiNotification;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define an OkHi class variable to be used for managing permissions and various other services
+    OkHi okhi;
+
     // Define an OkHiLocation that'll be used for verification
     final OkHiLocation workAddress = new OkHiLocation("NmUHW84306", -1.313339237582541, 36.842414181487776);
 
-    // Define your app context: OkHiMode.DEV | OkHiMode.SANDBOX | OkHiMode.PROD - dev will be removed in an update
+    // Define your app context: OkHiMode.SANDBOX | OkHiMode.PROD - dev will be removed in an update
     private static final OkHiAppContext context = new OkHiAppContext.Builder(Secret.OKHI_DEV_MODE).build();
 
     // Initialise OkHiAuth with your branchId, clientKey and your apps context
@@ -41,18 +46,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialise the okverify library onCreate, to enable requesting of permissions and services
+        // Initialise OkHi to enable requesting of permissions and services. Must be done onCreate
+        okhi = new OkHi(this);
+
+        // Initialise the okverify library. Must be done onCreate
         okVerify = new OkVerify.Builder(auth, this).build();
 
         // Should be invoked one time on app start.
         // (optional) OkHiNotification, use to start a foreground service to transmit verification signals to OkHi servers
+        int importance = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ? NotificationManager.IMPORTANCE_DEFAULT : 3;
         OkVerify.init(getApplicationContext(), new OkHiNotification(
                 "Verifying your address",
                 "We're currently verifying your address. This won't take long",
                 "OkHi",
                 "OkHi Address Verification",
                 "Alerts related to any address verification updates",
-                NotificationManager.IMPORTANCE_DEFAULT,
+                importance,
                 R.mipmap.ic_launcher
         ));
     }
@@ -89,12 +98,11 @@ public class MainActivity extends AppCompatActivity {
         OkVerify.stop(getApplicationContext(), workAddress.getId());
     }
 
-    class Handler implements OkVerifyRequestHandler {
+    class Handler implements OkHiRequestHandler<Boolean> {
         @Override
-        public void onSuccess() {
-            startAddressVerification();
+        public void onResult(Boolean result) {
+            if (result) startAddressVerification();
         }
-
         @Override
         public void onError(OkHiException exception) {
             showMessage(exception.getMessage());
@@ -105,14 +113,14 @@ public class MainActivity extends AppCompatActivity {
     private boolean canStartAddressVerification() {
         Handler requestHandler = new Handler();
         // Check and request user to enable location services
-        if (!OkVerify.isLocationServicesEnabled(getApplicationContext())) {
-            okVerify.requestEnableLocationServices(requestHandler);
-        } else if (!OkVerify.isGooglePlayServicesAvailable(getApplicationContext())) {
+        if (!OkHi.isLocationServicesEnabled(getApplicationContext())) {
+            okhi.requestEnableLocationServices(requestHandler);
+        } else if (!OkHi.isGooglePlayServicesAvailable(getApplicationContext())) {
             // Check and request user to enable google play services
-            okVerify.requestEnableGooglePlayServices(requestHandler);
-        } else if (!OkVerify.isLocationPermissionGranted(getApplicationContext())) {
+            okhi.requestEnableGooglePlayServices(requestHandler);
+        } else if (!OkHi.isLocationPermissionGranted(getApplicationContext())) {
             // Check and request user to grant location permission
-            okVerify.requestLocationPermission("Hey we need location permissions", "Pretty please..", requestHandler);
+            okhi.requestLocationPermission("Hey we need location permissions", "Pretty please..", requestHandler);
         } else {
             return true;
         }
@@ -128,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         // Pass permission results to okverify
-        okVerify.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        okhi.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -136,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         // Pass activity results results to okverify
-        okVerify.onActivityResult(requestCode, resultCode, data);
+        okhi.onActivityResult(requestCode, resultCode, data);
     }
 
     public void handleButtonTap(View view) {
