e96c7ce8b6310606127f2ea7e87c3b16c90bad43
Julius Kiano <jgkiano@gmail.com>
2024-06-12 09:10:27 +0300

    bump up version

--- android-okverify/src/main/java/io/okhi/android_okverify/models/Constant.java
+++ android-okverify/src/main/java/io/okhi/android_okverify/models/Constant.java
@@ -46,7 +46,7 @@ public class Constant {
         try {
             // library info
             String LIBRARY_NAME = "okverifyMobileAndroid";
-            String LIBRARY_VERSION = "1.9.43";
+            String LIBRARY_VERSION = "1.9.44";
             lib.put("name", LIBRARY_NAME);
             lib.put("version", LIBRARY_VERSION);
             meta.put("lib", lib);
